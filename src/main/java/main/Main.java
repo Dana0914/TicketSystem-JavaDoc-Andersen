e5f95f0ea8c2628f623bbd3ab290ee4d08e6b1b4
Dana <Dana.ackerman0914@gmail.com>
2024-06-27 09:23:13 +0500

    Tested in main function

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        BusTicket busTicket1 = new BusTicket(TicketType.WEEK, LocalDate.now(), new BigDecimal("120"), "1245");
+        BusTicket busTicket1 = new BusTicket(TicketType.WEEK, LocalDate.now(), new BigDecimal("1204"), "4586");
         BusTicket busTicket2 = new BusTicket(TicketType.DAY, LocalDate.of(2024, 7, 7), new BigDecimal("410"), "7856");
         BusTicket busTicket3 = new BusTicket(TicketType.MONTH, LocalDate.of(2024, 7, 15), new BigDecimal("200"), "8632");
         BusTicketService service = new BusTicketService();
